6878ea717ebc6e5158ed1028c174c971fd105154
AtotheY <anthony.sistilli@ryerson.ca>
2015-11-25 00:54:34 -0500

    Added test classes for more clear idea of how hiarchy in Intellij works vs Netbeans for George+Neethan

---
+++ src/Test2.java
@@ -0,0 +1,13 @@
+/**
+ * Created by AnthonyS on 11/25/2015.
+ */
+public class Test2 extends Test {
+
+    public static void main (String args[])
+    {
+        Test test = new Test2();
+        int response = test.value;
+
+        System.out.println ("The meaning of life is "+response);
+    }
+}
