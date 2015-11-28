99084988815046b63a8f142983903d021463ee10
AtotheY <anthony.sistilli@ryerson.ca>
2015-11-27 21:41:17 -0500

    Main file to test the API from DataPull

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created by AnthonyS on 11/27/2015.
+ */
+public class Main {
+
+    public static void main (String args[]) throws Exception
+    {
+        DataPull test = new DataPull("toronto");
+        //System.out.println(test.response())
+    }
+}
