67bf0de4cc3a1dddbca1c1944f96714c4fcebaf2
AtotheY <anthony.sistilli@ryerson.ca>
2015-12-05 17:16:18 -0500

    Changed MainRandomCompare to a fully accessible call function instead of a main runable file.

--- src/MainRandomCompare.java
+++ src/MainRandomCompare.java
@@ -6,67 +6,58 @@ import org.json.simple.JSONObject;
 
 //The main class which creates the city objects and will output to the user the city
     //name, city's weather and windspeed.
-public class Main {
+public class MainRandomCompare {
+    private CityGeneratorFactory factory = null;
+    private CityGeneratorFactory factory2 = null;
+    private CityGenerator x = null;
+    private CityGenerator y = null;
+    private double weather1 = 0;
+    private double weather2 = 0;
+    private double wind1 = 0;
+    private double wind2 = 0;
 
-    public static void main (String args[]) throws Exception {
-
-
-        ///creating the two city objects
-        CityGeneratorFactory factory = new CityGeneratorFactory();
-        CityGeneratorFactory factory2 = new CityGeneratorFactory();
-
-        //generating the two cities
+    public void setup() throws Exception{
+        //Generating cities
         CityGenerator x = factory.generateCity();
         CityGenerator y = factory2.generateCity();
 
         //instance variables which are equal to their corresponding values.
-        double weather1 = x.getWeather();
-        double weather2 = y.getWeather();
-        double wind1 = x.getWindSpeed();
-        double wind2 = y.getWindSpeed();
+        weather1 = x.getWeather();
+        weather2 = y.getWeather();
+        wind1 = x.getWindSpeed();
+        wind2 = y.getWindSpeed();
+    }
 
-        //Outputting the information to the user.
-        System.out.println("The First randomly generated City is "+ x.getName());
-        getInfo(x);
-
-        System.out.println(" ");
-
-        System.out.println("The Second randomly generated City is "+ y.getName());
-        getInfo(y);
-
-        //Comparing the weather and windspeed between the two cities.
+    public String randTempCompare()
+    {
         if(weather1>weather2)
         {
-            System.out.println(x.getName()+"'s temperature is greater than "+ y.getName()
-            +"'s temperature");
+           return(x.getName()+"'s temperature is greater than "+ y.getName()
+                    +"'s temperature");
         }
         else if(weather2>weather1){
-            System.out.println(y.getName() + "'s temperature is greater than " + x.getName()
+            return(y.getName() + "'s temperature is greater than " + x.getName()
                     + "'s temperature");
         }
         else
         {
-            System.out.println("Both cities have the same windspeed");
+            return("Both cities have the same temperature");
         }
+    }
 
+    public String randWindCompare()
+    {
         if(wind1>wind2)
         {
-            System.out.println(x.getName() + "'s windspeed is greater than " + y.getName()
+            return(x.getName() + "'s windspeed is greater than " + y.getName()
                     + "'s windspeed");
         }
         else if (wind2>wind1)
         {
-            System.out.println(y.getName() + "'s windspeed is greater than " + x.getName()
+            return(y.getName() + "'s windspeed is greater than " + x.getName()
                     + "'s windspeed");
         }
         else
-
-            System.out.println("Both cities have the same windspeed");
-    }
-    //the method which gets the message that will show the temperature and windspeeds.
-    public static void getInfo(CityGenerator city)
-    {
-        city.showMessageOne();
-
+            return("Both cities have the same windspeed");
     }
 }
