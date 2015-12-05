7ead4604b760e28f728f494093d1401601707efe
Neethan <s.neethan@hotmail.com>
2015-12-05 16:05:38 -0500

    Added example on how to use DataPull api as a library. This example gets the temperature of two cities, compares them, and outputs a description.

--- src/CityGeneratorFactory.java
+++ src/CityGeneratorFactory.java
@@ -2,10 +2,11 @@
  * Created by neethan on 2015-12-02.
  */
 
-//This class is the factory class which returns the RandomCity and RandomCity2 classes back.
+//This class is the factory class which returns the RandomCity class back.
 public class CityGeneratorFactory {
     public CityGenerator generateCity() throws Exception{
 
+        //return the created random city.
         return new RandomCity();
     }
 }
